8ce4c4d85972b822d8a353fc42a218d03f064d37
maggie.wan <maggie.wan@aiunion.com.tw>
2019-07-10 17:44:31 +0800

    my google map

---
+++ app/src/main/java/com/maggie/mygooglemap/MapsActivity.java
@@ -0,0 +1,52 @@
+package com.maggie.mygooglemap;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng TaipieEgg = new LatLng(25.0513481, 121.5486728);
+        LatLng Hospital= new LatLng(25.0477974, 121.5486488);
+        mMap.addMarker(new MarkerOptions().position(TaipieEgg).title("台北小巨蛋"));
+        mMap.addMarker(new MarkerOptions().position(Hospital).title("台安醫院"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(TaipieEgg,18));
+    }
+}
